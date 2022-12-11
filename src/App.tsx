78da3b5d25e5e1b7112aa27be9d7c54bee651f3f
EtErNaLiFe12 <wnsqja8888@gmail.com>
2022-12-11 19:48:22 +0900

    [JB] made animatedCursor with framter motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
 import Router from './routes';
+import SwiperCore, { Autoplay } from 'swiper';
+import AnimatedCursor from 'components/AnimatedCursor';
 
 function App() {
+  SwiperCore.use([Autoplay]);
+
   return (
     <>
       <Router />
+      <AnimatedCursor />
     </>
   );
 }
