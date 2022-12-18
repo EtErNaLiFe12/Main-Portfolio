ae1d59a5c7dde26a286e025ddd7ddb5006dc3389
EtErNaLiFe12 <wnsqja8888@gmail.com>
2022-12-18 21:26:02 +0900

    [JB] axios package installed & add GET -  google api address

---
+++ src/api/common.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import http from "utils/axios";
+
+
+// export async function test(): Promise<any> {
+//   const { data } = await http.get('https://sheets.googleapis.com/v4/spreadsheets/1sD_1s-BbrEoJwprkH-3RDgQSlHKN-wDKFh38oCjDY5E/values/A1:C3?majorDimension=COLUMNS&key=AIzaSyDLYgQeb_vtOARXbSzk_CSynK-9y6vgtl8');
+//   return data;
+// }
+
+// export function test() {
+//   const data = axios.get('https://sheets.googleapis.com/v4/spreadsheets/1sD_1s-BbrEoJwprkH-3RDgQSlHKN-wDKFh38oCjDY5E/values/A1:C3?majorDimension=COLUMNS&key=AIzaSyDLYgQeb_vtOARXbSzk_CSynK-9y6vgtl8');
+//   return data
+// }
