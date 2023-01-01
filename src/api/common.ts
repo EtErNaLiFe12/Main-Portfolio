e294afc2d97338b3505a0e44a4c21f00be7deaa7
EtErNaLiFe12 <wnsqja8888@gmail.com>
2023-01-01 18:25:34 +0900

    [JB] - test api added

--- src/api/common.ts
+++ src/api/common.ts
@@ -7,7 +7,7 @@ import http from "utils/axios";
 //   return data;
 // }
 
-// export function test() {
-//   const data = axios.get('https://sheets.googleapis.com/v4/spreadsheets/1sD_1s-BbrEoJwprkH-3RDgQSlHKN-wDKFh38oCjDY5E/values/A1:C3?majorDimension=COLUMNS&key=AIzaSyDLYgQeb_vtOARXbSzk_CSynK-9y6vgtl8');
-//   return data
-// }
+export function test() {
+  const data = axios.get('https://sheets.googleapis.com/v4/spreadsheets/1sD_1s-BbrEoJwprkH-3RDgQSlHKN-wDKFh38oCjDY5E/values/A1:C3?majorDimension=COLUMNS&key=AIzaSyDLYgQeb_vtOARXbSzk_CSynK-9y6vgtl8');
+  return data
+}
