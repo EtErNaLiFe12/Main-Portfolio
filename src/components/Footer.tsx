174cea533cc5399a690bd951e32f9443c8260a47
EtErNaLiFe12 <wnsqja8888@gmail.com>
2023-01-01 18:26:39 +0900

    [JB] - Footer component added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,17 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 function Footer() {
   return (
-    <Box sx={{ backgroundColor: '#555', width: '100%', height: 200 }}>Footer section</Box>
+    <Stack
+      flexDirection={'column'}
+      justifyContent={'center'}
+      alignItems={'center'}
+      sx={{ backgroundColor: '#555', width: '100%', height: 60 }}
+    >
+      <Typography variant="h6" color="#fff" sx={{ fontSize: 14 }}>
+        😀 ⚪️ Name: Jun Beom Park / Phone: 010-3143-4666 / Address: Shingil-dong 65-55
+        204ho ⚪️
+      </Typography>
+    </Stack>
   );
 }
 
